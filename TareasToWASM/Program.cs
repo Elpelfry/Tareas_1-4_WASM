2bfdd093aeb6a00eadf7b9d88acf2f2b1c7a3004
Delfry Paulino Ortega <delfry_paulino@ucne.edu.do>
2024-03-02 20:04:23 -0400

    Boostrap Icons and API URI

--- TareasToWASM/Program.cs
+++ TareasToWASM/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5286/api/") });
 
 await builder.Build().RunAsync();
